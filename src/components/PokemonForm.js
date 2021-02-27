b66e6895d91a6551a8d1c7a43c522f6d59a0a919
Josh Frank <eurosewusa@gmail.com>
2021-02-27 09:07:05 -0500

    commit tree 1d86750f078d04332db66bc3a19e30a6ff6f5ef9

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "semantic-ui-react";
 
-function PokemonForm() {
+function PokemonForm( { pokemon, setPokemon } ) {
+
+  const [ formState, setFormState ] = useState( { name: "", hp: "", frontUrl: "", backUrl: "" } );
+
+  function updateFormState( changeEvent ) {
+    const updatedFormState = { ...formState };
+    updatedFormState[ changeEvent.target.name ] = changeEvent.target.value;
+    setFormState( updatedFormState );
+  }
+
+  function addPokemon() {
+    const newPokemon = { name: formState.name, hp: formState.hp, sprites: { front: formState.frontUrl, back: formState.backUrl } };
+    fetch( "http://localhost:3001/pokemon", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify( newPokemon ) } )
+      .then( response => response.json() )
+      .then( newPokemonData => {
+        const updatedPokemon = [ ...pokemon, newPokemonData ];
+        setPokemon( updatedPokemon );
+      } );
+  }
+  
   return (
     <div>
       <h3>Add a Pokemon!</h3>
@@ -11,25 +30,28 @@ function PokemonForm() {
         }}
       >
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
+          <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={ updateFormState } />
+          <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={ updateFormState } />
           <Form.Input
             fluid
             label="Front Image URL"
             placeholder="url"
             name="frontUrl"
+            onChange={ updateFormState }
           />
           <Form.Input
             fluid
             label="Back Image URL"
             placeholder="url"
             name="backUrl"
+            onChange={ updateFormState }
           />
         </Form.Group>
-        <Form.Button>Submit</Form.Button>
+        <Form.Button onClick={ addPokemon }>Submit</Form.Button>
       </Form>
     </div>
   );
+  
 }
 
 export default PokemonForm;
